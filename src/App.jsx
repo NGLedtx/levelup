b1229b3f71ca21e4e4ddbe98fe9101f877c91f19
Natan Gabriel Lopes <naglopes2007@gmail.com>
2026-05-11 20:19:56 -0300

    fix: resolve mobile overflow with min-h-screen

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import NotFound from "./components/pages/NotFound";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="flex flex-col justify-center h-screen bg-gradient-to-br from-purple-900 via-gray-900 to-indigo-900">
+    <div className="flex flex-col justify-center bg-gradient-to-br from-purple-900 via-gray-900 to-indigo-900">
       <BrowserRouter>
         <Routes>
           <Route path="/profile" element={<Profile />} />
--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,13 +1,23 @@
-import { FaLightbulb, FaUser } from "react-icons/fa";
+import { FaBolt, FaUser } from "react-icons/fa";
 import ProfileHeader from "../profile/ProfileHeader";
 import BarXp from "../profile/BarXp";
 import StatCard from "../profile/StatCard";
+import { HiOutlineArrowTrendingUp } from "react-icons/hi2";
+import Achievements from "../profile/Achievements";
 
 function Profile() {
   return (
-    <div className="flex flex-col justify-center items-center bg-transparent h-screen">
-      <ProfileHeader />
-      <StatCard icon={FaLightbulb} text="Xp total" value="1000" />
+    <div className="flex flex-col items-center min-h-screen">
+      <ProfileHeader type="Mago" />
+      <div className="flex gap-5 mt-5 justify-center items-center">
+        <StatCard icon={<FaBolt className="text-yellow-500"/>} text="Xp Total" value="1000" />
+        <StatCard
+          icon={<HiOutlineArrowTrendingUp className="text-green-500"/>}
+          text="Sequência"
+          value="5 dias"
+        />
+      </div>
+      <Achievements/>
     </div>
   );
 }
