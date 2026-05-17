dc47df06e6d9f1127d09c7f573f00d402c8f2f18
Natan Gabriel Lopes <naglopes2007@gmail.com>
2026-05-17 10:44:50 -0300

    feat: add authentication form state and reusable components

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -1,4 +1,4 @@
-function Input({ type, text, placeholder, name, value, icon }) {
+function Input({ type, text, placeholder, name, value, icon, onChange}) {
   return (
     <div>
       <label htmlFor={name}>{text}</label>
@@ -8,6 +8,9 @@ function Input({ type, text, placeholder, name, value, icon }) {
           type={type}
           placeholder={placeholder}
           id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
           className="text-gray-800 pl-8 outline-none focus:ring-2 focus:ring-gray-500 rounded h-10"
           required
         />
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,5 +1,6 @@
 import Input  from "../form/Input.jsx";
 import Button from "../form/Button.jsx";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { FaLock, FaUserPlus } from "react-icons/fa";
 
@@ -8,7 +9,29 @@ import { MdEmail } from "react-icons/md";
 import { LuSwords } from "react-icons/lu";
 
 function Login() {
+  const navigate = useNavigate();
   const [isLogin, setIsLogin] = useState(true);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword:""
+  })
+  function handleChange(e){
+    const { name, value} = e.target
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }))
+  }
+  function handleSubmit(e){
+    e.preventDefault();
+    localStorage.setItem(
+      "user",
+      JSON.stringify(formData)
+    )
+  }
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-950 via-slate-900 to-amber-950 flex flex-col items-center justify-center px-4 relative overflow-hidden text-white">
       <div className="bg-gradient-to-r from-amber-400 via-yellow-300 to-amber-500 rounded-full p-5">
@@ -22,7 +45,8 @@ function Login() {
       <div className=" flex justify-center flex-col items-center bg-gray-800/50 rounded-lg p-8">
         <h2 className="text-2xl">{isLogin ? "Bem vindo de volta" : "Criar uma conta"}</h2>
         <p className="text-sm text-gray-400">{isLogin ? "O retorno do Herói ":"É hora de transformar a sua vida"}</p>
-        <form className="flex flex-col p-4 gap-3">
+
+        <form className="flex flex-col p-4 gap-3" onSubmit={handleSubmit}>
           <div>
               <Input
               type="text"
@@ -30,6 +54,8 @@ function Login() {
               name="name"
               placeholder="Digite seu nome"
               icon={<FaUserPlus/>}
+              value={formData.name}
+              onChange={handleChange}
               />
             </div>
           {!isLogin && (
@@ -40,6 +66,8 @@ function Login() {
               name="email"
               placeholder="Digite seu email"
               icon={<MdEmail/>}
+              value={formData.email}
+              onChange={handleChange}
               />
             </div>
           )}
@@ -50,6 +78,8 @@ function Login() {
               name="password"
               placeholder="Crie uma senha"
               icon={<FaLock/>}
+              value={formData.password}
+              onChange={handleChange}
               />
           </div>
           {!isLogin && (
@@ -57,9 +87,11 @@ function Login() {
               <Input
               type="password"
               text="Confirme a senha"
-              name="password"
+              name="confirmPassword"
               placeholder="Confirme sua senha"
               icon={<FaLock/>}
+              value={formData.confirmPassword}
+              onChange={handleChange}
               />
             </div>
           )}
